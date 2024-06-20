f81e6ff9dda318645fdab8fd34ec2a2625949a20
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-06-20 11:27:44 +0200

    base app changed

--- frontend/angular-app/src/app/app.routes.ts
+++ frontend/angular-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: HomepageComponent,
+        title: "HiveMind"
+    },{
+        path: "login",
+        component: LoginComponent,
+        title: "Login"
+    },{
+        path: "",
+        redirectTo: "/home",
+        pathMatch: 'full'
+      },
+];
