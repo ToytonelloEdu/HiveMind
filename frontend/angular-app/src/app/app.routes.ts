dc37b26921026dbc78e5f2c2e1b4ee4d0e75e058
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-06-27 12:45:22 +0200

    new routes

--- frontend/angular-app/src/app/app.routes.ts
+++ frontend/angular-app/src/app/app.routes.ts
@@ -1,16 +1,40 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ConnectPageComponent } from './connect-page/connect-page.component';
+import { IdeaDetailsComponent } from './idea-details/idea-details.component';
+import { authGuard } from './_guards/auth/auth.guard';
 
 export const routes: Routes = [
     {
         path: "home",
         component: HomepageComponent,
-        title: "HiveMind"
+        title: "HiveMind",
+        canActivate: [authGuard]
     },{
         path: "login",
         component: LoginComponent,
-        title: "Login"
+        title: "Login | HiveMind"
+    },{
+        path: "signup",
+        component: SignupComponent,
+        title: "Signup | HiveMind"
+    },{
+        path: "logout",
+        component: LogoutComponent,
+        title: "Logout | HiveMind"
+    },{
+        path: "connect",
+        component: ConnectPageComponent,
+        title: "Connect | HiveMind",
+        canActivate: [authGuard]
+    },{
+        path: "ideas/:id",
+        component: IdeaDetailsComponent,
+        title: "Idea | HiveMind",
+        canActivate: [authGuard]
     },{
         path: "",
         redirectTo: "/home",
