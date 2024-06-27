9310d70b7ccdf6b8acd7b892819b2556be59a6ec
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-06-27 12:45:13 +0200

    added providers

--- frontend/angular-app/src/app/app.config.ts
+++ frontend/angular-app/src/app/app.config.ts
@@ -1,8 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { authInterceptor } from './_interceptors/auth/auth.interceptor';
+import { provideMarkdown, MarkdownModule } from 'ngx-markdown';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(), 
+    provideToastr({
+      progressBar: true,
+    }),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor])
+    ),
+    provideMarkdown({
+      
+    }),
+    MarkdownModule,
+    provideRouter(routes)
+  ]
 };
