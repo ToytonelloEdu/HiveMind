30d7cd5abc9386418558aaa3e9be99cb3e241637
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-06-27 12:37:18 +0200

    added connect button

--- frontend/angular-app/src/app/footer/footer.component.ts
+++ frontend/angular-app/src/app/footer/footer.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../_services/auth/auth.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+   authService = inject(AuthService);
+   router = inject(Router);
 
+   ngOnInit() {
+    console.log(this.router.url);
+    
+   }
 }
