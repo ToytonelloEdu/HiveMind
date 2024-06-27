ca3b48aa23fe0632467e6995c463647797e2bdeb
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-06-27 12:47:29 +0200

    idea details

---
+++ frontend/angular-app/src/app/idea-details/idea-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-idea-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './idea-details.component.html',
+  styleUrl: './idea-details.component.scss'
+})
+export class IdeaDetailsComponent {
+
+}
