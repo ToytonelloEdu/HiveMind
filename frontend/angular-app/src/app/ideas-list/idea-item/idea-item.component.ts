1ff6d39ca108c935ce17addfb20a46fe823c763a
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-07-10 20:36:43 +0200

    idea-item changes

--- frontend/angular-app/src/app/ideas-list/idea-item/idea-item.component.ts
+++ frontend/angular-app/src/app/ideas-list/idea-item/idea-item.component.ts
@@ -4,9 +4,10 @@ import { RestApiService } from '../../_services/rest-api/rest-api.service';
 import { RouterLink } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { MarkdownComponent, MarkdownService } from 'ngx-markdown';
+import { AuthService } from '../../_services/auth/auth.service';
 
 
-enum VoteState { none, upvote, downvote};
+export enum VoteState { none, upvote, downvote};
 
 @Component({
   selector: 'app-idea-item',
@@ -17,16 +18,30 @@ enum VoteState { none, upvote, downvote};
 })
 export class IdeaItemComponent {
   @Input({required: true}) ideaItem: IdeaItem;
-  mdService = inject(MarkdownService);
-  
+  @Output() voteClicked = new EventEmitter;
 
   ideaLink = "";
   restapi = inject(RestApiService);
   toastr = inject(ToastrService);
+  authService = inject(AuthService);
+  mdService = inject(MarkdownService);
   markdown = "";
 
   ngOnInit() {
-    this.ideaLink = "/todos/"+this.ideaItem?.id;
+    this.ideaLink = "/ideas/"+this.ideaItem?.id;
+    const id = this.ideaItem.id as number;
+    const user = this.authService.user() as string;
+    this.restapi.getVote(id, user).subscribe({
+      next: vote => {
+        if(vote) {
+          if(vote.upvote) {
+            this.voteState = VoteState.upvote
+          } else {
+            this.voteState = VoteState.downvote
+          }
+        }
+      }
+    })
     if(this.ideaItem.isMarkDown) {
       this.markdown = this.mdService.parse(this.ideaItem.description).toString();
     }
@@ -38,27 +53,35 @@ export class IdeaItemComponent {
   voteUploading = false;
 
   handleUpvote() {
+    let change = false;
     if(this.voteState !== VoteState.upvote && !this.voteUploading ){
       this.voteUploading = true;
       if(this.voteState === VoteState.downvote) {
         this.ideaItem.downvotes--;
+        change = true;
       }
       this.voteState = VoteState.upvote;
       this.ideaItem.upvotes++;
       this.restapi.updateVotes(this.ideaItem).subscribe({
         error: (err) => {
           this.toastr.error("Could not upvote idea");
-        },
-        complete: () => {this.voteUploading = false;}
+        }
       });
+      if(change){
+        this.putVote(true);
+      } else {
+        this.postVote(true);
+      }
     }
   }
 
   handleDownvote() {
+    let change = false;
     if(this.voteState !== VoteState.downvote && !this.voteUploading ) {
       this.voteUploading = true;
       if(this.voteState === VoteState.upvote) {
         this.ideaItem.upvotes--;
+        change = true;
       }
       this.voteState = VoteState.downvote;
       this.ideaItem.downvotes++;
@@ -69,7 +92,31 @@ export class IdeaItemComponent {
         complete: () => {this.voteUploading = false;}
       });
     }
-    
+    if(change){
+      this.putVote(false);
+    } else {
+      this.postVote(false);
+    }
+  }
+
+  private putVote(upvote: boolean) {
+    this.restapi.putVote({
+      upvote: upvote,
+      IdeaId: this.ideaItem.id as number,
+      UserUsername: this.authService.user() as string
+    }).subscribe({
+      complete: () => { this.voteUploading = false; this.voteClicked.emit()}
+    });
+  }
+
+  private postVote(upvote: boolean) {
+    this.restapi.postVote({
+      upvote: upvote,
+      IdeaId: this.ideaItem.id as number,
+      UserUsername: this.authService.user() as string
+    }).subscribe({
+      complete: () => { this.voteUploading = false; this.voteClicked.emit()}
+    });
   }
 
 }
