23ee1bb153ceb7c2fea1a5e97addd4bcccad1c1e
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-07-10 20:36:51 +0200

    ideas-list changes

--- frontend/angular-app/src/app/ideas-list/ideas-list.component.ts
+++ frontend/angular-app/src/app/ideas-list/ideas-list.component.ts
@@ -1,17 +1,18 @@
-import { Component, inject } from '@angular/core';
+import { Component, Signal, computed, inject, signal } from '@angular/core';
 import { IdeaItem } from '../_services/rest-api/idea-item.type';
 import { RestApiService } from '../_services/rest-api/rest-api.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { IdeaItemComponent } from './idea-item/idea-item.component';
-import { isEmpty } from 'rxjs';
+import { NgFor } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 type fetchType = "main" | "popular" | "unpopular";
 
 @Component({
   selector: 'app-ideas-list',
   standalone: true,
-  imports: [IdeaItemComponent],
+  imports: [NgxPaginationModule, IdeaItemComponent],
   templateUrl: './ideas-list.component.html',
   styleUrl: './ideas-list.component.scss'
 })
@@ -22,6 +23,8 @@ export class IdeasListComponent {
   router = inject(Router);
 
   ideas: IdeaItem[] = []
+  totalLength: any;
+  page: number = 1;
 
   currentMode: fetchType = "main";
 
@@ -67,6 +70,7 @@ export class IdeasListComponent {
     this.restapi.getIdeas().subscribe({
       next: (data) => {
         this.ideas = data;
+        this.totalLength = this.ideas.length
       },
       error: (err) => {
         this.handleFetchError(err);
@@ -78,6 +82,7 @@ export class IdeasListComponent {
     this.restapi.getPopularIdeas().subscribe({
       next: (data) => {
         this.ideas = data;
+        this.totalLength = this.ideas.length
       },
       error: (err) => {
         this.handleFetchError(err);
@@ -89,6 +94,7 @@ export class IdeasListComponent {
     this.restapi.getUnpopularIdeas().subscribe({
       next: (data) => {
         this.ideas = data;
+        this.totalLength = this.ideas.length
       },
       error: (err) => {
         this.handleFetchError(err);
